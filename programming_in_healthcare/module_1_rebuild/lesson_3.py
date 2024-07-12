6503d9829ea3e460661dfed41d1f8ee8752edb9b
Raman Sharma <raman0503@hotmail.com>
2024-07-12 14:56:54 +0100

    Changed Toml

--- programming_in_healthcare/module_1_rebuild/lesson_3.py
+++ programming_in_healthcare/module_1_rebuild/lesson_3.py
@@ -21,3 +21,23 @@ https://docs.streamlit.io/library/advanced-features/theming
 3. Change the font
 4. Change theme
 """
+import streamlit as st
+
+def calc_kh(knee_ht):
+    ht=(2.69*knee_ht)+24.2
+    return ht
+
+def main():
+    st.title("Knee Height calculator")
+
+    knee_ht=st.number_input("Knee height in cm",step=1)
+    age = st.number_input("Age:", step=1)
+    gender = st.selectbox("Gender:", ["Other", "Male", "Female"])
+    try:
+        ht=calc_kh(knee_ht)
+    except:
+        st.write("Awaiting inputs")    
+    st.write(f"The calculated height is {ht:.1f} cm")    
+
+if __name__ == "__main__":
+    main()
